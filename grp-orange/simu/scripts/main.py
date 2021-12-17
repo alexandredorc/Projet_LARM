e4cd64f878ecfe24ae4bbb143b0958b24d7cd075
alexandre dorcival <ddorcival@gmail.com>
2021-12-17 09:24:41 +0100

    feat :  add the changing controling

--- grp-orange/simu/scripts/main.py
+++ grp-orange/simu/scripts/main.py
@@ -29,9 +29,27 @@ def callB(data):
             aPoint= [math.cos(angle) * aDistance, math.sin( angle ) * aDistance]
             obstacles.append( aPoint )
         angle+= data.angle_increment
-    rospy.loginfo( str(
+    """rospy.loginfo( str(
         [ [ round(p[0], 2), round(p[1], 2) ] for p in  obstacles[0:10] ] 
-    ) + " ..." )
+    ) + " ..." )"""
+    dist_min=100
+    angle_min=0
+    for aDistance in data.ranges :
+        if 0.1 < aDistance and aDistance < 0.5 :
+            if dist_min > angle:
+                dist_min=aDistance
+                angle_min=angle
+        angle+= data.angle_increment
+    speed=0.5
+    if angle_min > 0:
+        spin= -1
+    if angle_min < 0:
+        spin= 1
+    cmd= Twist()
+    cmd.angular.z= spin
+    cmd.linear.x= speed
+    commandPublisher.publish(cmd)
+
 
 rospy.Subscriber("/base_scan", LaserScan, callB )
 # spin() enter the program in a infinite loop
