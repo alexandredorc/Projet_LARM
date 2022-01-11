fced6c67ea26bdb403d8e02cb3f7128ee5c370c7
poirotman <alexis.poirot19@gmail.com>
2022-01-11 16:29:49 +0100

    clean code HSV improvement black bottle detection

--- grp-orange/scripts/vision/HSV_detec.py
+++ grp-orange/scripts/vision/HSV_detec.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 import math, rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseStamped
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 import message_filters
 from test_marker import *
-
-rospy.init_node('image_test', anonymous=True)
+import tf
 
 def souris(event, x, y, flags, param):
     global lo, hi, color, hsv_px
-    sensi=np.array([50,100,50])
+    sensi=np.array([15,15,15])
 
     if event == cv2.EVENT_MOUSEMOVE:
         # Conversion des trois couleurs RGB sous la souris en HSV
         global frame
+        print(x,y)
         px = frame[y,x]
         px_array = np.uint8([[px]])
         hsv_px = cv2.cvtColor(px_array,cv2.COLOR_BGR2HSV)
@@ -31,91 +31,119 @@ def souris(event, x, y, flags, param):
     lo=np.amax(np.vstack((np.array(color),min_color)),axis=0)-sensi
     hi=np.amin(np.vstack((np.array(color),max_color)),axis=0)+sensi
 
-   
-bridge = CvBridge()
-
-
 def realCoor(x,y,sc_x,sc_y,D):
-    
     width=43.5*sc_x/640
     angle=43.5*(x-640)/640
     dist=D
     angle=angle*math.pi/180
     return [math.cos(angle) * dist, math.sin( angle ) * dist-35]
 
-def callback(data):
-    
-    global frame
-    frame = bridge.imgmsg_to_cv2(data, "bgr8")
-    
-    global image
-    image=cv2.blur(frame, (7, 7))
+def display_info(rec,x,y):
+    cv2.rectangle(frame, (int(rec[0]), int(rec[1])), (int(rec[0])+int(rec[2]), int(rec[3])+int(rec[1])), color_info, 2)
+    cv2.circle(frame, (int(x), int(y)), 5, color_info, 10)
+    cv2.line(frame, (int(x), int(y)), (int(x)+150, int(y)), color_info, 2)
+    cv2.putText(frame, "Objet !!!", (int(x)+10, int(y) -10), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
+            
 
-    global depth_data
-    
-    #480 848  240 424
-    mask=cv2.inRange(image, lo, hi)
-    mask=cv2.erode(mask, None, iterations=4)
-    mask=cv2.dilate(mask, None, iterations=4)
-    image2=cv2.bitwise_and(image, frame, mask= mask)
-    cv2.putText(frame, "Couleur: {0} {1} {2}ts.registerCallback(callback)".format(color[0],color[1],color[2]), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
-    
-    # Affichage des compdata(255, 255, 255), 1, cv2.LINE_AA)
-
-    elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-
-
-    if len(elements) > 0:
-        c=max(elements, key=cv2.contourArea)
-        rec=cv2.boundingRect(c)
-        x=int(rec[0]+(rec[2])/2)
-        y=int(rec[1]+(rec[3])/2)
-        depth=depth_data[y][x]
-        coor= realCoor(x,y,rec[2],rec[3],depth)
-        print("depth",depth/10)
-        print(coor)
-        rayon=500
-        if rayon>30 and depth != 0:
-            cv2.rectangle(frame, (int(rec[0]), int(rec[1])), (int(rec[0])+int(rec[2]), int(rec[3])+int(rec[1])), color_info, 2)
-            cv2.circle(frame, (int(x), int(y)), 5, color_info, 10)
-            cv2.line(frame, (int(x), int(y)), (int(x)+150, int(y)), color_info, 2)
-            cv2.putText(frame, "Objet !!!", (int(x)+10, int(y) -10), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
-            marker_publish(coor[0]/100,-coor[1]/100,0.5)
-
-
-
+def display_images():
+    stencil=cv2.bitwise_and(image, frame, mask= mask)
+    cv2.putText(frame, "Couleur: {0} {1} {2}".format(color[0],color[1],color[2]), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
     cv2.imshow('Camera', frame)
-    cv2.imshow('image2', image2)
+    cv2.imshow('Stencil', stencil)
     cv2.imshow('Mask', mask)
 
     cv2.waitKey(3)
 
+
+def image_proc(data):
+    print("Receive data from camera")
+    global frame
+    global image
+    global mask
+    global depth_data
+
+    frame = bridge.imgmsg_to_cv2(data, "bgr8")
+    image=cv2.blur(frame, (7, 7))
+    mask=cv2.inRange(image, lo, hi)
+    mask=cv2.erode(mask, None, iterations=8)
+    mask=cv2.dilate(mask, None, iterations=8)
+
+    elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    close_elem=[]
+    for e in elements:
+        rec=cv2.boundingRect(e)
+        x=int(rec[0]+(rec[2])/2)
+        y=int(rec[1]+(rec[3])/2)
+        depth_bottle=depth_data[y,x]
+        print(cv2.contourArea(e))
+        if depth_bottle >= 150 and depth_bottle < 1500:
+            close_elem.append(e)
+
+    if len(close_elem) > 0 and depth_data is not None:
+        
+        bottle=max(close_elem, key=cv2.contourArea)
+        rec=cv2.boundingRect(bottle)
+        x=int(rec[0]+(rec[2])/2)
+        y=int(rec[1]+(rec[3])/2)
+
+        depth_bottle=depth_data[y,x]
+        coor= realCoor(x,y,rec[2],rec[3],depth_bottle)
+
+        if  depth_bottle >= 150 and depth_bottle < 1500:
+            display_info(rec,x,y)
+            gestionBottle(coor[0]/100,-coor[1]/100)
+            
+    display_images()
+
     rate.sleep()
 
+def gestionBottle(x,y):
+    global tfListener, bottles
+    createPose = init_PoseStamped(x,y)
+    transfPose = tfListener.transformPose("map", createPose  )
+    test = 0
+    for id,aBottle in enumerate(bottles):
+        if math.sqrt((transfPose.pose.position.x-aBottle[0])**2 + (transfPose.pose.position.y-aBottle[1])**2) < 1.5 :
+            transfPose.pose.position.x=(transfPose.pose.position.x+aBottle[0])/2
+            transfPose.pose.position.y=(transfPose.pose.position.y+aBottle[1])/2
+            marker_modify(transfPose.pose.position.x,transfPose.pose.position.y,transfPose.pose.position.z,id )
+            aBottle[0]=transfPose.pose.position.x
+            aBottle[1]=transfPose.pose.position.y
+            test=1
+   
+    if test == 0:
+        bottle=[transfPose.pose.position.x,transfPose.pose.position.y]
+        bottles.append(bottle)
+        marker_add(transfPose.pose.position.x,transfPose.pose.position.y,transfPose.pose.position.z, len(bottles))
 
-def depth_CB(data):
+5
+def get_depth(data):
     global depth_data
     depth_data = np.array(bridge.imgmsg_to_cv2(data, desired_encoding="passthrough"))
 
+if __name__=="__main__":
+
+    print("(ง`_´)ง")#(ง`_´)ง
+
+    rospy.init_node('image_proc', anonymous=True)
     
-color=[25,25,25]
-depth_data=np.array([])
-lo=np.array(color)-10
-hi=np.array(color)+10
-color_info=(0, 0, 255)
 
-cv2.namedWindow('Camera')
-cv2.setMouseCallback('Camera', souris)
-hsv_px = [0,0,0]
+    bridge = CvBridge()
+    tfListener= tf.TransformListener()
 
-rate=rospy.Rate(10)
+    bottles=[]
+    color=[15,20,20]
+    hsv_px = [0,0,0]
+    color_info=(0, 0, 255)
+    depth_data=None
+    lo=np.array(color)-10
+    hi=np.array(color)+10
 
-rospy.Subscriber("/camera/color/image_raw", Image, callback )
+    cv2.namedWindow('Camera')
+    cv2.setMouseCallback('Camera', souris)
 
-rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image , depth_CB)
+    rospy.Subscriber("/camera/color/image_raw", Image, image_proc)
+    rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image , get_depth)
 
-
-print("(ง`_´)ง")#(ง`_´)ง
-
-# spin() enter the program in a infinite loop
-rospy.spin() 
+    rate=rospy.Rate(10) # spin() enter the program in a infinite loop
+    rospy.spin() 
--- grp-orange/scripts/vision/test_marker.py
+++ grp-orange/scripts/vision/test_marker.py
@@ -3,26 +3,26 @@ from geometry_msgs.msg import Twist
 import cv2
 import numpy as np
 from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PoseStamped
 
 commandPublisher = rospy.Publisher(
     '/bottle',
     Marker, queue_size=10
 )
 
-def _init_markers(x,y,z):
+def _init_markers(x,y,z,id):
     print("test")
     marker = Marker()
-    marker.header.frame_id = 'camera_link' #self.global_frame
+    marker.header.frame_id = 'map' #self.global_frame
     marker.header.stamp=rospy.Time.now()
     marker.ns= "marker"
-    #marker.id= 0
+    marker.id= id
     marker.type = 1
     marker.action = Marker.ADD
     marker.pose.position.x= x
     marker.pose.position.y= y
     marker.pose.position.z= z
-    marker.lifetime= rospy.Duration.from_sec(10)
+    marker.lifetime= rospy.Duration.from_sec(0)
     marker.pose.orientation.x = 0.0
     marker.pose.orientation.y = 0.0
     marker.pose.orientation.z = 0.0
@@ -31,15 +31,31 @@ def _init_markers(x,y,z):
     marker.color.g = 1.0
     marker.color.b = 0.0
     marker.color.a = 1.0
-    marker.scale.x = 1
-    marker.scale.y = 1
-    marker.scale.z = 1.0
+    marker.scale.x = 0.2
+    marker.scale.y = 0.2
+    marker.scale.z = 0.4
     return marker
 
-def marker_publish(x,y,z):
-    bottle= _init_markers(x,y,z)
-    commandPublisher.publish(bottle)
+def init_PoseStamped(x,y):
+    Pose = PoseStamped()
+    Pose.header.frame_id = 'camera_link'
+    # Pose.header.stamp.secs= 0
+    Pose.pose.position.x= x
+    Pose.pose.position.y= y
+    Pose.pose.position.z= 0
+    Pose.pose.orientation.x = 0.0
+    Pose.pose.orientation.y = 0.0
+    Pose.pose.orientation.z = 0.0
+    Pose.pose.orientation.w = 1.0
+    return Pose
 
+def marker_add(x,y,z,id):
+    bottle= _init_markers(x,y,z,id)
+    commandPublisher.publish(bottle)
+def marker_modify(x,y,z,id):
+    bottle= _init_markers(x,y,z,id)
+    bottle.action = Marker.MODIFY
+    commandPublisher.publish(bottle)
 
  
 
