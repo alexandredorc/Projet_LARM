50c0a022907bf7a658d98b15568467588b7cf475
bot@vincent <imt-mobisyst@drods.net>
2022-01-20 20:56:40 +0100

    final presentation

--- grp-orange/scripts/controler.py
+++ grp-orange/scripts/controler.py
@@ -3,16 +3,15 @@ from geometry_msgs.msg import Twist
 
 class Controler: 
     def __init__(self):
-        self.speed=0.5
+        self.speed=0.2
         self.speed_goal=0.5
-        self.spin=0
         self.spin_goal=0
 
     def check_path(self, angle_min, dist_min): # cette fonction determine si le robot peut passer dans un chemin devant lui
         aPoint_G= [math.cos(angle_min[0]) * dist_min[0], math.sin( angle_min[0] ) * dist_min[0]]
         aPoint_D= [math.cos(angle_min[1]) * dist_min[1], math.sin( angle_min[1] ) * dist_min[1]]
         dist= math.sqrt((aPoint_G[0]-aPoint_D[0])**2+(aPoint_G[1]-aPoint_D[1])**2)
-        if (dist<0.35):
+        if (dist<0.25):
             self.spin_goal=1
             self.speed_goal=0.03
         else:
@@ -25,6 +24,7 @@ class Controler:
             self.speed-=0.02
         if(self.speed<  self.speed_goal):
             self.speed+=0.02
+        
         cmd.angular.z= self.spin_goal
         cmd.linear.x= self.speed
         """ print("twist")
